inducer/barvinok

commit 96c72099f250716cee20922c3f8aa72d8b04f518

iscc.c: use isl_stream_is_empty instead of reading eof field of isl_stream
Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

iscc.c
@@ -2562,7 +2562,7 @@ static __isl_give isl_printer *source_file(struct isl_stream *s,
 
 	register_named_ops(s_file);
 
-	while (!s_file->eof)
+	while (!isl_stream_is_empty(s_file))
 		p = read_line(s_file, table, p, 0);
 
 	isl_stream_free(s_file);
@@ -2598,7 +2598,7 @@ int main(int argc, char **argv)
 
 	install_signal_handler(ctx);
 
-	while (p && !s->eof) {
+	while (p && !isl_stream_is_empty(s)) {
 		isl_ctx_resume(ctx);
 		p = read_line(s, table, p, tty);
 	}

commit aabc0f35f4fcfb39b1ddb735e1c24aedeace071b

iscc: allow interruption of computation from the keyboard again
Support for interrupting the computation was added in e2d8fba (iscc:
allow interruption of computation from the keyboard,
Thu Mar 17 10:07:22 2011 +0100), but broken in 96c7209 (iscc.c:
use isl_stream_is_empty instead of reading eof field of isl_stream,
Wed Dec 17 17:30:25 2014 +0100) because the subsequent call
to isl_stream_is_empty would think the stream is empty because
no token could be constructed.

Move the detection of interruption to the main loop, also handling
the case of interruption during input and combining it with the call
to isl_ctx_resume.
Handling of interruption during input is not ideal because the first
token after the interrupt is dropped.

Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

iscc.c
@@ -2648,10 +2648,6 @@ static __isl_give isl_printer *read_line(struct isl_stream *s,
 
 	obj = read_expr(s, table);
 	ctx = isl_stream_get_ctx(s);
-	if (isl_ctx_last_error(ctx) == isl_error_abort) {
-		fprintf(stderr, "Interrupted\n");
-		isl_ctx_reset_error(ctx);
-	}
 	if (isl_stream_eat(s, ';'))
 		goto error;
 
@@ -2808,9 +2804,19 @@ int main(int argc, char **argv)
 
 	install_signal_handler(ctx);
 
-	while (p && !isl_stream_is_empty(s)) {
-		isl_ctx_resume(ctx);
-		p = read_line(s, table, p, tty);
+	while (p) {
+		int empty;
+
+		empty = isl_stream_is_empty(s);
+		if (empty && isl_ctx_last_error(ctx) != isl_error_abort)
+			break;
+		if (!empty)
+			p = read_line(s, table, p, tty);
+		if (isl_ctx_last_error(ctx) == isl_error_abort) {
+			fprintf(stderr, "Interrupted\n");
+			isl_ctx_resume(ctx);
+			isl_ctx_reset_error(ctx);
+		}
 	}
 
 	remove_signal_handler(ctx);