9f65999927b33b002d6bf55a0fd5acd2df1f01ee
Sven Verdoolaege <skimo@kotnet.org>
2016-10-21 12:54:23 +0200

    drop references to piplib submodule
    This was missing from 9d3250e (drop piplib submodule,
    Fri Dec 24 22:40:19 2010 +0100).
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- barvinok/options.h
+++ barvinok/options.h
@@ -73,10 +73,8 @@ struct barvinok_options {
     int			    try_Delaunay_triangulation;
 
     /* basis reduction options */
-    #define	BV_GBR_PIP	0
     #define	BV_GBR_GLPK	1
     #define	BV_GBR_CDD	2
-    #define	BV_GBR_PIP_DUAL	3
     #define	BV_GBR_ISL	4
     int		gbr_lp_solver;
 
