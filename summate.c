48a39b660ab3ba4db6612628480f76e6d265ced8
Sven Verdoolaege <skimo@kotnet.org>
2016-12-19 16:28:41 +0100

    summate.c: directly include required headers
    Do so instead of relying on the headers getting included indirectly.
    
    Signed-off-by: Sven Verdoolaege <skimo@kotnet.org>

--- summate.c
+++ summate.c
@@ -1,7 +1,11 @@
+#include <isl/space.h>
+#include <isl/set.h>
 #include <isl/map.h>
 #include <isl/union_set.h>
 #include <isl/union_map.h>
+#include <isl/polynomial.h>
 #include <isl_set_polylib.h>
+#include <barvinok/polylib.h>
 #include <barvinok/options.h>
 #include <barvinok/util.h>
 #include "bernoulli.h"
